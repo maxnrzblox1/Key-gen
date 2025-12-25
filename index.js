20bed81ad27a08a268894cda6be38604a191a5b4
maxnrzblox1 <maxnrzblox@gmail.com>
2025-12-25 18:13:18 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
   res.setHeader("Content-Type", "text/plain");
-  res.status(200).send("https://example.com");
+  res.status(200).send("https://565680ce-031a-4fdf-8ea6-acb883d17c9c-00-2ca090bn1uq71.pike.replit.dev/api/code");
 }
